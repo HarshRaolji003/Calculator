3d0102adef83049c3d35dac990107a69f5fa954a
HarshRaolji003 <148987860+HarshRaolji003@users.noreply.github.com>
2024-05-28 19:20:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-let display= document.querySelector('.display');
-let value = document.querySelector('#btn1')
-
-display.innerText="hello world";
-console.log(value);
+// Calculator logic comes here
