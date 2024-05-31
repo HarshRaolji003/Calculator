f72dc5bbb1d71a5dc80dae672cc484ad79305148
HarshRaolji003 <148987860+HarshRaolji003@users.noreply.github.com>
2024-05-31 19:11:20 +0530

    javascript file

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// Calculator logic comes here
+const result=document.querySelector('.result');
+
+function appendToDisplay(input){
+    result.innerHTML +=input;
+}
+
+function Clear(){
+
+}
+ 
+function ClearScreen(){
+    result.innerHTML="";
+}
+
+function Calculate(){
+    try{
+        result.innerHTML=eval(result.innerHTML)
+    } catch(error){
+        result.innerHTML="Error";
+    }
+}
